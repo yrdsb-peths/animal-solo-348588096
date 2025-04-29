108f4f3a73dff6d2909c62d750929dc301446eba
Stewart Chan <dev.codepet@gmail.com>
2025-04-29 14:10:43 -0400

    Set apple image

--- Apple.java
+++ Apple.java
@@ -9,10 +9,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Apple extends Actor
 {
+    public Apple() {
+        setImage("images/apple1.png");    
+    }
+    
     public void act()
     {
         setLocation(getX(), getY()+1);
-        setImage("apple1");
+        
         MyWorld world = (MyWorld) getWorld();
         if(getY() >= world.getHeight())
         {
