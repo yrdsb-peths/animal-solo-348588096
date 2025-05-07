719150dd088b1cb912d49c053a2c2389f049e777
348588096 <veznup@gmail.com>
2025-05-07 14:10:07 -0400

    Add Timer and Movement

--- MyWorld.java
+++ MyWorld.java
@@ -8,6 +8,8 @@ public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1, false );
         
+        setBackground("images/background.jpg");
+        
         SeaHorse seahorse = new SeaHorse();
         addObject(seahorse,300,200);
         
--- SeaHorse.java
+++ SeaHorse.java
@@ -4,35 +4,68 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class SeaHorse extends Actor
 {
     GreenfootSound seahorseSound = new GreenfootSound("seahorse.mp3");
-    GreenfootImage[] idle = new GreenfootImage[4];
+    GreenfootImage[] idleRight = new GreenfootImage[4];
+    GreenfootImage[] idleLeft = new GreenfootImage[4];
+    
+    SimpleTimer animationTimer = new SimpleTimer();
+    String facing = "right";
+    
     public SeaHorse() {
-        for(int i=0 ; i<idle.length ; i++)
+        for(int i=0 ; i<idleRight.length ; i++)
         {
-            idle[i] = new GreenfootImage("images/idle" + i +".png"); 
+            idleRight[i] = new GreenfootImage("images/idle" + i +".png");
+            idleRight[i].scale(100,50);
         }
-        setImage(idle[0]);
+        
+        for(int i = 0; i < idleLeft.length; i++)
+        {
+            idleLeft[i] = new GreenfootImage("images/idle" + i +".png");
+            idleLeft[i].mirrorHorizontally(); 
+            idleLeft[i].scale(100,50);
+        }
+        
+        setImage(idleRight[0]);
+        animationTimer.mark();
     }
     
     int imageIndex = 0; 
+  
+    
     public void animateSeahorse()
     {
-        setImage(idle[imageIndex]);
-        imageIndex = (imageIndex + 1) % idle.length;
+        if (animationTimer.millisElapsed()< 1)
+        {
+            return ;
+        }
         
+        if(facing.equals("right"))
+        {
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;
+        }
+        else 
+        {
+            setImage(idleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleLeft.length;
+        }
+            
     }
     
     public void act()
     {
         if(Greenfoot.isKeyDown("left")){
             move(-5);
+            facing = "left";
         }
         else if(Greenfoot.isKeyDown("right")){
             move(5);
+            facing = "right";
         }
         
         eat();
         animateSeahorse();
     }
+    
     
     public void eat(){
         if(isTouching(Apple.class)){
